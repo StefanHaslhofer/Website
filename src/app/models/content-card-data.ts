737ee0a1e6ab212aef493c0f2ddb8d4c5bd5dccb
StefanHaslhofer <haslhofer.stefan@gmail.com>
2021-05-05 19:18:35 +0200

    missing changes

--- src/app/models/content-card-data.ts
+++ src/app/models/content-card-data.ts
@@ -1,12 +1,16 @@
 export class ContentCardData {
 
+  type: string;
   description: string;
   pictureUrl: string;
+  pictureUrls: string[];
 
   deserialize(input: any): ContentCardData {
     if (input) {
+      this.type = input.type;
       this.description = input.description;
       this.pictureUrl = input.pictureUrl;
+      this.pictureUrls = input.pictureUrls;
     }
 
     return this;
--- src/app/picture-card/picture-card.component.ts
+++ src/app/picture-card/picture-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ContentCardData} from '../models/content-card-data';
 
 @Component({
   selector: 'app-picture-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picture-card.component.scss']
 })
 export class PictureCardComponent implements OnInit {
+
+  @Input()
+  cardData: ContentCardData;
 
   constructor() { }
 
